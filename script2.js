5315d5c0b9704dca90878d9c2f828b3d091fbea8
Glitch Out <88343612+CallMeGlitch@users.noreply.github.com>
2021-12-09 14:10:56 -0600

    Add files via upload

--- script2.js
+++ script2.js
@@ -1469,3 +1469,72 @@ function getDiv(container) {
     return document.createElement(node);
     return element;
 };
+
+
+function createNode(node) {
+    let element = document.createElement(node);
+    return element;
+};
+
+function displayItems(items, container) {
+    let items_container = getDiv(container);
+    items_container.innerHTML = '';
+
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+
+        let item_node = createNode("li");
+        item_node.setAttribute("id", item.id);
+
+        if (item.count > 0) {
+            item_node.innerHTML = `{item.name}
+            <span id="badge">${item.count}</span>`;
+            appendNode(items_container, item_node);
+        }
+    }
+}
+
+displayItems(items_array, "Items");
+
+function addOrRemoveItemsFromCart(action) {
+    let container = '';
+    if (action == "add") {
+        container = getDiv("Items");
+
+        takeAction(container)
+    }
+    else if (action == "reomve") {
+        container = getDiv("cart");
+        
+        takeAction(container)
+    };
+
+    function takeAction(container) {
+        container.addeventListener("click", function(event) { 
+            let item_id = event.target.id;
+
+            if (item_id !== "items" && item_id !== "badge") {
+                let item = items_array.filter(function (item) {
+                    return item.id == item_id;
+                })[0];
+
+                let item_in_cart = cart.filter(function (item) {
+                    return item.id == item_id;
+                })[0];
+
+                if (item_in_cart == undefined) {
+                    cart.push(item);
+                }
+                else if (action == "add") {
+                    item_in_cart.count++;
+                }
+                else if (action == "remove") {
+                    item_in_cart.count--;
+                };
+            };
+        });
+    };
+}
+
+addOrRemoveItemsFromCart('add');
+addOrRemoveItemsFromCart('remove');
